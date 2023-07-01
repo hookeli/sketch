dbda42d0af20ed500bf3a46ad7d7364662f15aaf
hookeli <61351348+hookeli@users.noreply.github.com>
2023-07-02 00:35:04 +0800

    Add files via upload

--- test01_streamlit.py
+++ test01_streamlit.py
@@ -6,8 +6,8 @@ st.title('welcome to the world of Nanitalk')
 with st.sidebar:
     choose = option_menu('Nanitalk', ['虎克', '纳尼说', '呆鸟日记'], icons = ['house', 'cloud-upload', 'list-task'], menu_icon="cast")
     if choose == '虎克':
-        st.image('https://raw.githubusercontent.com/hookeli/sketch/main/images/WX20230701-174954%402x.png', caption='caption', use_column_width=True)
+        st.image('https://cdn.jsdelivr.net/gh/hookeli/sketch@main/images/WX20230701-174840%402x.png', caption='caption', use_column_width=True)
     elif choose == '纳尼说':
-        st.image('https://raw.githubusercontent.com/hookeli/sketch/main/images/WX20230701-174840%402x.png', caption='caption', use_column_width=True)
+        st.image('https://cdn.jsdelivr.net/gh/hookeli/sketch@main/images/WX20230701-174938%402x.png', caption='caption', use_column_width=True)
     else:
-        st.image('https://raw.githubusercontent.com/hookeli/sketch/main/images/WX20230701-174938%402x.png', caption='caption', use_column_width=True)
+        st.image('https://cdn.jsdelivr.net/gh/hookeli/sketch@main/images/WX20230701-174954%402x.png', caption='caption', use_column_width=True)
